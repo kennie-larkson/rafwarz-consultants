403b190b0ace977a26679eecd4c3593880c9a92d
kennie-larkson <materialkenny@gmail.com>
2025-01-03 23:47:42 +0100

    added home link to subscription form

--- app/subscribe/page.tsx
+++ app/subscribe/page.tsx
@@ -1,9 +1,16 @@
+import Link from "next/link";
 import { SubscriptionForm } from "../components/forms/subscription-form";
 
 export default function SubscribePage() {
   return (
     <div className="min-h-screen py-12 bg-gray-50 dark:bg-gray-900">
       <div className="container mx-auto px-4">
+        <Link
+          href="/"
+          className="text-blue-500 hover:underline mb-4 inline-block w-full pl-36"
+        >
+          &larr; Back to Home
+        </Link>
         <SubscriptionForm />
       </div>
     </div>
